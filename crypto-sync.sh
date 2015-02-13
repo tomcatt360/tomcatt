1482c3d23dce4b2e91895da53c01834802d8582e
Thomas Parker <tomcatt360@gmail.com>
2015-02-13 12:51:32 -0700

    Change sleep times to

--- crypto-sync.sh
+++ crypto-sync.sh
@@ -1,31 +1,33 @@
 #!/bin/bash
+
+#a script to 
 echo "Bitcoin sync in progress..."
 ./bitcoin-qt &
-sleep 900
+sleep $1
 echo "killing bitcoin..."
 killall bitcoin-qt
 
 echo "Namecoin sync in progress..."
 namecoin &
-sleep 900
+sleep $1
 echo "killing namecoin"
 killall namecoin
 
 echo "litecoin sync in progress..."
 ./litecoin-qt &
-sleep 900
+sleep $1
 echo "killing litecoin..."
 killall litecoin-qt
 
 echo "peercoin sync in progress"
 ./ppcoin-qt &
-sleep 900
+sleep $1
 echo "killing peercoin"
 killall ppcoin-qt
 
 echo "primecoin sync in progress"
 ./primecoin-qt &
-sleep 900
+sleep $1
 echo "killing primecoin"
 killall primecoin-qt
 
