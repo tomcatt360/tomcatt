b798304382e6e93f93863645dbcd256743fe4543
Thomas Parker <tomcatt360@gmail.com>
2015-04-17 07:56:06 -0500

    pass switches to upgrade and dist-upgrade
    useful if you want to pass a switch to upgrade and dist-upgrade e.g. -y

--- upgrade.sh
+++ upgrade.sh
@@ -3,9 +3,9 @@
 sudo echo "Updating package lists (update)"
 sudo apt-get update
 echo "Upgrading packages (upgrade)"
-sudo apt-get upgrade
+sudo apt-get $1 upgrade
 echo "Upgrading System packages (dist-upgrade)"
-sudo apt-get dist-upgrade
+sudo apt-get $1 dist-upgrade
 echo "Cleaning up (autoclean)"
 sudo apt-get autoclean
 echo "Done."
